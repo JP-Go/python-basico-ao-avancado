4c502f933f8e18fdb1ff7306891e2ffb6e66caf9
João Pedro <jpedro@gmail.com>
2022-01-10 17:42:02 -0300

    Aula de métodos_de_classes

---
+++ modulo_3_poo/aulas/metodos_de_classes.py
@@ -0,0 +1,25 @@
+class Pessoa:
+    ano_atual = 2021
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def get_ano_nascimento(self):
+        print(self.ano_atual - self.idade)
+
+    def __repr__(self) -> str:
+        return f"Nome: {self.nome}; Idade: {self.idade}"
+
+    # Método que pertence somente a classe Pessoa, e não aos objetos
+    # Esse método de criar um objeto da classe pessoa só faz sentido
+    # para a classe e não para o objeto
+    @classmethod
+    def por_ano_nascimento(cls, nome, ano_nascimento):
+        # cls representa a classe
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)  # chamada da classe, criando um novo objeto
+
+
+p1 = Pessoa.por_ano_nascimento("Luiz", 1998)
+print(p1)
