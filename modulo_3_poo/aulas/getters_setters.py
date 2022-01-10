bb3e947e86e5b7aa848edd9453f76dd49cf4f5e8
João Pedro <jpedro@gmail.com>
2022-01-10 18:26:23 -0300

    Aula de getters e setters em python e de métodos estáticos

---
+++ modulo_3_poo/aulas/getters_setters.py
@@ -0,0 +1,45 @@
+def number_from_str(string: str):
+    digits_and_decimal_point = [
+        char for char in string if char.isdigit() or char == ','
+    ]
+    return float(''.join(digits_and_decimal_point).replace(',', '.'))
+
+
+class Produto:
+
+    def __init__(self, nome: str, preco) -> None:
+        self.nome = nome
+        self.preco = preco
+
+    def desconto(self, percentual: float) -> None:
+        self.preco = self.preco * (1 - (percentual / 100))
+
+    # Getter
+    @property
+    def preco(self):
+        return self._preco
+
+    # Setter
+    @preco.setter
+    def preco(self, valor):
+        if (isinstance(valor, str)):
+            valor = number_from_str(valor)
+        self._preco = valor
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome.title()
+
+
+p1 = Produto('camisa', 50)
+p1.desconto(10)
+
+p2 = Produto('CHINELO', 'R$ 20,00')
+p2.desconto(10)
+
+print(p1.nome, p1.preco)
+print(p2.nome, p2.preco)
