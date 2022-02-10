154a36bde552dc5d46a228fc509fcf67522fe343
João Pedro <jpedro@gmail.com>
2022-02-10 14:05:06 -0300

    Conserto no algoritmo de conversão de Ip e renomeação de classes

--- poo/exercicios/calculadora_de_subredes/converter.py
+++ poo/exercicios/calculadora_de_subredes/converter.py
@@ -1,6 +1,9 @@
 class NumberConverter:
+    """Class that defines a tool to convert number from decimal to byte and vice-versa"""
+
     @staticmethod
     def convert_int_to_byte(integer: int) -> str:
+        """Converts a decimal integer to its byte corresponding number"""
         if not isinstance(integer, int):
             raise ValueError("Value has to be an integer")
         if integer < 0:
@@ -12,8 +15,8 @@ class NumberConverter:
 
     @staticmethod
     def convert_byte_to_int(byte: str) -> int:
+        """Converts a byte number to its decimal integer number"""
         byte = byte.replace(".", "")
-        print(byte)
         if not isinstance(byte, str):
             raise ValueError("Value has to be an string")
         if not byte.isnumeric():
@@ -21,14 +24,18 @@ class NumberConverter:
         return int(byte, 2)
 
 
-class IpRepresentationConverter:
+class IpConverter:
+    """Class desingned to convert ip addresses from one representation to another"""
+
     @staticmethod
-    def to_byte_representation(ip: str) -> str:
+    def to_byte_repr(ip: str) -> str:
+        """Converts a ipv4 ip address from its decimal representation to byte representation"""
         convert_to_byte = NumberConverter().convert_int_to_byte
         byte_groups = [convert_to_byte(int(group)) for group in ip.split(".")]
         return ".".join(byte_groups)
 
     @staticmethod
-    def to_decimal_representation(ip: str) -> str:
-        decimal_groups = [str(int(group)) for group in ip.split(".")]
+    def to_decimal_repr(ip: str) -> str:
+        """Converts a ipv4 ip from its byte representation to decimal representation"""
+        decimal_groups = [str(int(group, 2)) for group in ip.split(".")]
         return ".".join(decimal_groups)
