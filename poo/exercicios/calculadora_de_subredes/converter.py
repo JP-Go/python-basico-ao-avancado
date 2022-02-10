f80ec15dad9aebc0c1745903296bd0e2beaee94d
João Pedro <jpedro@gmail.com>
2022-02-10 11:33:58 -0300

    Inicio da resolução do desafio e calculadora de subredes Ipv4

--- poo/exercicios/calculadora_de_subredes/converter.py
+++ poo/exercicios/calculadora_de_subredes/converter.py
@@ -12,8 +12,23 @@ class NumberConverter:
 
     @staticmethod
     def convert_byte_to_int(byte: str) -> int:
+        byte = byte.replace(".", "")
+        print(byte)
         if not isinstance(byte, str):
             raise ValueError("Value has to be an string")
         if not byte.isnumeric():
             raise ValueError("Value has to be a string composed of ones and zeroes")
         return int(byte, 2)
+
+
+class IpRepresentationConverter:
+    @staticmethod
+    def to_byte_representation(ip: str) -> str:
+        convert_to_byte = NumberConverter().convert_int_to_byte
+        byte_groups = [convert_to_byte(int(group)) for group in ip.split(".")]
+        return ".".join(byte_groups)
+
+    @staticmethod
+    def to_decimal_representation(ip: str) -> str:
+        decimal_groups = [str(int(group)) for group in ip.split(".")]
+        return ".".join(decimal_groups)
--- poo/exercicios/calculadora_de_subredes/main.py
+++ poo/exercicios/calculadora_de_subredes/main.py
@@ -1,1 +1,28 @@
-DECIMAIS = [10, 20, 12, 45]
+from converter import NumberConverter
+
+
+class CalcIpv4:
+    """Classe que, dado um ip base e uma máscara de ip no protocolo Ipv4, calcula informações sobre a rede Ipv4"""
+
+    def __init__(self, ip: str, mascara: str):
+        self.ip = ip
+        self.mascara = mascara
+        self.numero_de_ips = self.calcular_numero_de_ips()
+
+    def calcular_ip_de_rede(self):
+        pass
+
+    def calcular_ip_de_broadcast(self):
+        pass
+
+    def calcular_numero_de_ips(self):
+        pass
+
+    def calcula_primeiro_ip(self):
+        pass
+
+    def calcula_ultimo_ip(self):
+        pass
+
+
+print(NumberConverter.convert_byte_to_int("0000.0000.0000.0001"))
